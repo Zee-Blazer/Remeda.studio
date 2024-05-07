494f8125ca0bde67e5c1adc656c07969916e41a0
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-05-07 11:03:53 +0100

    Remeda.studio website

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.get("*", (req, res) => {
+    res.render("404.html", {error: "No page"})
+})
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Server is running on port:${port}`));
