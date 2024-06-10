7c3a774e45c7931823ad96e6d6222d4aaee75631
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-06-10 17:28:53 +0100

    Updated the header of contact screen

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+// App use instances
+app.use(express.json());
+app.use(cors());
+
 app.use(express.static(path.join(__dirname, "./public")));
+
+app.post('/send-email', async (req, res) => {
+    
+})
 
 app.get("*", (req, res) => {
     res.render("404.html", {error: "No page"}) // To send a 404 page
